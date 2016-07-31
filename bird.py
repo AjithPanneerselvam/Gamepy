c2a287b59b816e0b6496435eb516a03b2d786176
wahid_abdul <awahidag@gmail.com>
2016-07-31 13:01:16 +0530

    Add files via upload

--- bird.py
+++ bird.py
@@ -1,23 +1,17 @@
 import pygame
 stop = False
-screen=pygame.display.set_mode((1600,800))
-img=pygame.image.load(r"t1.png")
-img1=pygame.image.load(r"t2.png")	
-shadow = pygame.image.load(r'shadow.jpg')	
-white = (255,255,255)
-screen.fill(white)
-pygame.draw.line(screen, (0, 0, 0), (400, 800), (400,0))
-pygame.draw.line(screen, (0, 0, 0), (1200, 800), (1200,0))
-screen.blit(shadow,(700,650))
+screen=pygame.display.set_mode((800,600))
+img=pygame.image.load(r"p1.png")
+img1=pygame.image.load(r"p2.png")	
 while stop == False :
 	for event in pygame.event.get():
 		k=k=pygame.key.get_pressed()	
 		if event.type == pygame.QUIT or k[pygame.K_ESCAPE]:
 			stop =True
-	screen.blit(img ,(100,400));
+	screen.blit(img ,(0,0));screen.blit(img,(640,0))
 	pygame.display.flip()
-	pygame.time.wait(100)
-	screen.blit(img1,(1400,400))				
+	pygame.time.wait(200)
+	screen.blit(img1,(0,0))	;screen.blit(img1,(640,0))			
 	pygame.display.flip()
 	pygame.time.wait(100)
 	
